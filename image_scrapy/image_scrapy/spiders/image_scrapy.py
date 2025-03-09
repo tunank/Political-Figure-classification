cc4659b114e321041104bef095287a112aebcb8e
tunank <davidnguyen55535@gmail.com>
2025-03-09 18:05:35 -0400

    Added pagination into the scraper

--- image_scrapy/image_scrapy/spiders/image_scrapy.py
+++ image_scrapy/image_scrapy/spiders/image_scrapy.py
@@ -1,14 +1,21 @@
 import scrapy
 from image_scrapy.items import ImageScrapyItem
 
+
 class ImageScrapySpider(scrapy.Spider):
     name = 'image_scrapy'
-    start_urls = ['https://www.gettyimages.ca/photos/elon-musk-potrait']
+    search_term = "elon-musk-portrait"  # Fixed typo in "portrait"
+    page_number = 2
 
     # customize setting to restrict the number of images to download
     custom_settings = {
-        'CLOSESPIDER_ITEMCOUNT': 200
+        'CLOSESPIDER_ITEMCOUNT': 80,
+        'CLOSESPIDER_PAGECOUNT': 10  # Added page limit as a backup
     }
+
+    def start_requests(self):  # FIXED: Method name with 's'
+        url = f"https://www.gettyimages.ca/photos/{self.search_term}?page={self.page_number}"
+        yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response, **kwargs):
         all_images = response.xpath('//picture/img/@src').getall()
@@ -16,3 +23,7 @@ class ImageScrapySpider(scrapy.Spider):
         item = ImageScrapyItem()
         item['image_urls'] = all_images
         yield item
+
+        self.page_number += 1
+        next_page = f"https://www.gettyimages.ca/photos/{self.search_term}?page={self.page_number}"
+        yield response.follow(url=next_page, callback=self.parse)
